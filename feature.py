8a3065d968c0db98816cb4219e5416846f72d16c
Sphephelo Mlungwana <sphe.mlu@gmail.com>
2023-04-15 21:27:01 +0200

    added a valuation function

--- feature.py
+++ feature.py
@@ -1,1 +1,14 @@
-print("FEATURE")
+# this is a function
+
+def main(n,m):
+  if n > m:
+    return n , " is greater"
+  elif m > n:
+    return m , " is greater"
+  else:
+    return "these values are equal!"
+
+x = eval(input("Enter the first value: "))
+y = eval(input("Enter the first value: "))
+
+print(main(x,y))
